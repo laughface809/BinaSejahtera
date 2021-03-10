cc11c4e6cd68eedd7c1fefa6c182a861ea11a2a5
laughface809 <akhmarullah.techno@gmail.com>
2021-03-10 17:59:55 +0800

    tambah loading saat fetch data order

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f1d899f6b7913ff4fb96";
+/******/ 	var hotCurrentHash = "dcf9ed0c7f9077c0b9c2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/account.js
+++ pages/account.js
@@ -6,11 +6,13 @@ import {API_URL} from "../utils/urls";
 
 const useOrders = (user, getToken) => {
     const [orders, setOrders] = useState([])
+    const [loading, setLoading] = useState(false)
 
     useEffect(() =>{
         const fetchOrders = async ()=>{
             if(user){
                 try {
+                    setLoading(true)
                     const token = await getToken()
                     const order_res = await fetch(`${API_URL}/orders`,{
                         headers: {
@@ -23,18 +25,19 @@ const useOrders = (user, getToken) => {
                 catch (err){
                     setOrders([])
                 }
+                setLoading(false)
             }
         }
         fetchOrders()
         },[user])
-    return orders
+    return {orders, loading}
 }
 
 export default function Account(){
 
     const {user, logoutUser, getToken} = useContext(AuthContext)
 
-    const orders = useOrders(user, getToken)
+    const {orders, loading} = useOrders(user, getToken)
     console.log("Account.render orders", orders)
 
     if(!user){
@@ -55,6 +58,7 @@ export default function Account(){
 
             <h2>Account Page</h2>
             <h3>Your Orders</h3>
+            {loading && <p>Loading your orders</p>}
             {orders.map(order=>(
                 <div key={order.id}>
                     {new Date(order.createdAt).toLocaleDateString('en-MY')} {order.bs_product.name} ${order.total} {order.status}
